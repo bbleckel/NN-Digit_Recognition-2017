401df1fc270d3f64e81dc63363668708f9137300
jasperhouston <JasperHouston@dhcp-209-110.wireless.bowdoin.edu>
2017-04-23 22:42:47 -0400

    printInfo

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <cstdlib>
+#include <unistd.h>
+#include <iostream>
+#include <algorithm>
+#include "PSO.h"
+
+using namespace std;
+
+void printInfo() {
+	cout << endl;
+	cout << "Usage:\nFor Digit Recognition with a Neural Network:\n./main trainingFile testFile inputNodes outputNodes epochs learningRate" << endl;
+	cout << "    trainingFile   = name of training file from which to read (string):" << endl;
+	cout << "    testFile       = name of test file from which to read (string)" << endl;
+	cout << "    inputNodes     = number of input nodes (int)" << endl;
+	cout << "    outputNodes    = number of output nodes (int)" << endl;
+	cout << "    epochs         = number of epochs (iterations) to perform (int)" << endl;
+    cout << "    learningRate   = learning rate for network (double)" << endl;
+	cout << endl;
+}
+
+
+int main (int argc, char** argv) {
+
+}
