67bb1019615036f0f14b309f359c69ee97e8421b
bbleckel <lbleckel@bowdoin.edu>
2017-04-23 22:48:59 -0400

    Skeleton for header

---
+++ NN.h
@@ -0,0 +1,29 @@
+#ifndef NN_H
+#define NN_H
+
+#include <cstdlib>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <math.h>
+
+using namespace std;
+
+class inputNode {
+public:
+private:
+};
+
+class outputNode {
+public:
+private:
+};
+
+class network {
+public:
+    NeuralNetwork();
+    ~NeuralNetwork();
+private:
+};
+#endif
